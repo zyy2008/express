45a1db6aa89d4886e7086d84e09de3fe2f9c37a7
zyy <zyy2008@vip.qq.com>
2020-12-06 22:40:21 +0800

    tijiao

---
+++ datainit/restaurant.js
@@ -0,0 +1,27 @@
+db.getCollection("restaurant").insert({
+
+    _id: ObjectId("5fcaf1d402200000cb007a43"),
+
+    borough: "西安",
+
+    building: "2",
+
+    coord: "2",
+
+    cuisine: "土豆丝",
+
+    grades: 10,
+
+    name: "餐厅1",
+
+    owner: "zyy",
+
+    photo: "",
+
+    photo_mimetype: "",
+
+    street: "1",
+
+    zipcode: "2"
+
+});
---
+++ datainit/user.js
@@ -0,0 +1,5 @@
+db.getCollection("user").insert( {
+    _id: ObjectId("5fca55a13f5d000077004c3b"),
+    password: "123456",
+    username: "demo"
+} );
